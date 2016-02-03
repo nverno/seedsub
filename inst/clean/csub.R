d06dd3fb43ae4496283e2a193c97c70a5b82e1ab
Noah Peart <noah.v.peart@gmail.com>
2016-02-03 01:59:52 -0500

    did a bunch of cleaning on the seesap master

---
+++ inst/clean/csub.R
@@ -0,0 +1,10 @@
+### cseed.R --- 
+## Filename: cseed.R
+## Description: Cleaning contour substrate data submas99c to csub
+## Author: Noah Peart
+## Created: Tue Feb  2 17:07:06 2016 (-0500)
+## Last-Updated: Tue Feb  2 20:48:18 2016 (-0500)
+##           By: Noah Peart
+######################################################################
+## Notes:
+## - `ON` variables don't actually have name 'ON'
