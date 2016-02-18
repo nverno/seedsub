2e250c570b401a2d24a2552081d5aa7e223e5d44
Noah Peart <noah.v.peart@gmail.com>
2016-02-18 07:17:21 -0500

    ext growth cleaned

--- data-raw/clean/cextgr_clean.R
+++ data-raw/clean/cextgr_clean.R
@@ -3,7 +3,7 @@
 ## Description: Clean extension growth data
 ## Author: Noah Peart
 ## Created: Fri Feb  5 16:13:50 2016 (-0500)
-## Last-Updated: Wed Feb 17 05:52:57 2016 (-0500)
+## Last-Updated: Thu Feb 18 07:14:08 2016 (-0500)
 ##           By: Noah Peart
 ## */
 
@@ -23,14 +23,19 @@ opts_chunk$set(cache=FALSE, echo=TRUE, message=FALSE)
 
 library(data.table)
 library(stringi)
+library(DT)
+
 ## /* end setup */
 ##'
 ##+load-data, echo=FALSE
 ## Loading data, extract some variables/years
 load('../temp/cextgr.rda')
+setDT(cextgr)
+if (!inherits(cextgr, "data.table"))
+  cextgr <- as.data.table(cextgr)
 
 ## Describe these variables
-nms <- unique(stri_extract_first(names(cextgr), regex="[A-Z]+[0-9]?[A-Z]+"))
+## nms <- unique(stri_extract_first(names(cextgr), regex="[A-Z]+[0-9]?[A-Z]+"))
 
 ## Column groups
 nms <- names(cextgr)
@@ -50,7 +55,6 @@ patts <- list(                        # patterns to match groups
   exsums  = '^EXSUM[0-9]+'
 )
 ## others  = '^EX(?:CNT|SUM)[0-9]+'    # probably gonna ignore these
-
 
 allCols <- lapply(patts, grep, nms, perl=TRUE, value=TRUE)
 if (!all(nms %in% unlist(allCols))) stop( 'Missed some columns.' )
@@ -100,25 +104,37 @@ consts <- with(allCols, c(id, consts))
 ##' Going to transform each variable separately and merge back together, since the 
 ##' years to melt by vary amongst all the variables and I cant think of a better way.
 ##'
+##' ## Special Transforms
+##'
 ##' Some of the variables require special treatment: 
 ##'
-##'   + `NL_`, the names need to be stripped of 99 and then merged only for 99.
+##'   + `NL_` will become an indicator variable, 1 where a new leader was determined.
 ##'   + `PEX` variables need to merged with `EX` and an indicator column used for 
 ##' partial growth instead.  Check for conflicts where we have both partial and full
 ##' extension measurements.
 ##' 
-##+transform, echo=FALSE
+##+special-transforms, echo=FALSE
+################################################################################
+##
+##                                    EX
+##
+################################################################################
 ## Melt EX first, then join each additional melted variable to it
 id <- allCols$id
-res <- melt(cextgr[, c(id, allCols$exs), with=FALSE], id.vars=id,
+res <- data.table::melt(cextgr[, c(id, allCols$exs), with=FALSE], id.vars=id,
   na.rm=TRUE, value.name="EX", measure.vars=allCols$exs,
   variable.name="YEAR")
 res[, YEAR := stri_extract(YEAR, regex='[0-9]+')]
 
 ## We have extension growth measure at least once in all years in the range
 ## 1944 - 1998:
-tst <- all(diff(as.numeric(rownames(table(res$YEAR)))) == 1L)
+stopifnot(all(diff(as.numeric(rownames(table(res$YEAR)))) == 1L))
 
+################################################################################
+##
+##                                    PEX
+##
+################################################################################
 ## Check that `PEX` doesn't overlap with `EX` variables
 exs <- substr(allCols$pexs, 2, nchar(allCols$pexs))
 inds <- exs %in% names(cextgr)
@@ -130,150 +146,130 @@ tst <- lapply(seq_along(exs), function(i) {
 stopifnot(all(unlist(tst) == 0))
 
 ## Merge PEXs with EXs and add an indicator variable `PEX`
-pexs <- cextgr[, allCols$pexs, with=FALSE]
+pexs <- data.table::melt(cextgr[, c(id, allCols$pexs), with=FALSE],
+  id.vars=id, measure.vars=allCols$pexs, na.rm=TRUE,
+  value.name="EX", variable.name="YEAR")
+pexs[, PEX := 1]                                       # add indicator column
+pexs[, YEAR := stri_extract(YEAR, regex='[0-9]+')]     # clean YEAR
+res <- merge(res, pexs, by=c("ID", "YEAR"), all=TRUE)  # merge
 
-## list of easy varying variables (remove ENOTE99)
-allvary <- allCols[c("age", "minages", "terms", "d1rms", "d2rms", "enotes",
-  "excnts", "exsums")]
+## Check EX.x and EX.y don't overlap
+stopifnot(res[!is.na(EX.x) & !is.na(EX.y), .N] == 0L)
+
+## merge EX columns into one, remove others
+res[, EX := ifelse(!is.na(EX.x), EX.x, EX.y)][, c("EX.x", "EX.y") := NULL]
+
+## Should only have PEX in 89, 98, 99
+stopifnot(all(res[!is.na(PEX), .N, by=YEAR][, 
+  YEAR] %in% c("89", "98", "99")))
+
+################################################################################
+##
+##                                    NL
+##
+################################################################################
+## New leader variables from ENOTE99, in NL99_[1-3] columns
+## - Melt then shorten years to merge
+nl99s <- data.table::melt(cextgr[, c(id, allCols$nl99s), with=FALSE],
+  id.vars=id, measure.vars=allCols$nl99s, na.rm=TRUE,
+  value.name="YEAR", variable.name="NL", variable.factor=FALSE)
+nl99s[, NL := "1"]                     # indicator
+nl99s[, YEAR := as.character(YEAR)]
+nl99s[, YEAR := sub("^19", '', YEAR)]  # shorten year to merge
+
+## Sanity checks:
+## Should have one tree with 3 new leaders
+stopifnot(nl99s[, .N, by=ID][, max(N)] == 3L)
+stopifnot(nl99s[, .N, by=ID][N == 3L, .N] == 1L)
+
+## Merge into res
+## Note: there are 4 cases where there is no ext. growth measured,
+## but a new leader was determined (all 1999 except one 1995)
+res <- merge(res, nl99s, by=c(id, "YEAR"), all=TRUE) 
+
+## /* end special-treatment */
+##'
+
+##'
+##' ## Regular Transforms
+##' 
+##' The following will be melted, the YEAR extracted, then merged back into the 
+##' resulting long dataset.
+##'
+##' **Columns to transform**:
+##'
+##'   + `ENOTE[0-98]`: All of these except for `ENOTE99` go to long.  `ENOTE99` has different
+##' info that has already been split up into the `NL` columns (new leaders)
+##'   + `AGE88`: Total age (88)
+##'   + `MINAGE[0-9]+`: Minimum ages (88, 89, 98, 99).
+##'   + `TERM[0-9]+`: Terminal conditions (99, 00)
+##'   + `D[1-2]RM[0-9]+`: Distance remaining (98, 99)
+##'
+##+regular-transforms, echo=FALSE
+## list of varying variables (remove ENOTE99)
+allvary <- allCols[c("age", "minages", "terms", "d1rms", "d2rms", "enotes")]
 allvary$enotes <- setdiff(allvary$enotes, "ENOTE99")
 
 ## For each element, melt by those variables, clean the year column,
 ## and join back to `res`
-yrs <- unique(res$YEAR)
+yrs <- unique(res$YEAR)  # should alread have all possible years
 for (v in allvary) {
   coln <- stri_extract(v[[1]], regex='^[A-Z]+[1-2]?[A-Z]+', mode='first')
-  tmp <- melt(cextgr[, c(id, v), with=FALSE],
+  tmp <- data.table::melt(cextgr[, c(id, v), with=FALSE],
     id.vars=id, na.rm=TRUE, value.name=coln, measure.vars=v,
     variable.name='YEAR')
   tmp[, YEAR := stri_replace_first_fixed(YEAR, coln, '')]
-  res <- if (all(unique(tmp$YEAR) %in% yrs)) {
-    tmp[res, on=c(id, 'YEAR')]                     # simple join on ID and YEAR
-  } else {
-tst <- merge(res, tmp, by=c(id, 'YEAR'), all=TRUE)  # blank rows / new years
-  }
+  res <- merge(res, tmp, by=c(id, 'YEAR'), all=TRUE)  # many variables have no EX
 }
 
-## Add constants
-res <- cseed[, consts, with=FALSE][res, on=ids]
+## Add constants (YRREX)
+res <- cextgr[, consts, with=FALSE][res, on=id]
 
-## Add a unique plot identifier
-res[,PID := .GRP, by=c('CONTNAM', 'STPACE')]
+## /* end regular-transforms */
+##'
 
-## Put some of the columns up front, soil cols at the back
+##' 
+##' ## Cleanup
+##'
+##' + Rearrange column order for convenience
+##' + Clean the YEAR column to be full years
+##' + Convert types to integer where possible
+##' + Convert `YRREX` to indicator after years are cleaned up
+##'
+##+cleanup, echo=FALSE
+## Column ordering
 coln <- c(  # these will go up front for visual convenience
-  'PID', 'ID', 'CONTNAM', 'STPACE', 'SPEC', 'HT', 'SUB', 'SUBON', 'YEAR', 
-  'YRTAG', 'ALONG', 'DISUPDN', 'YRMORT', 'STAT', 'DECM', 'ELEVCL', 
-  'ASPCL', 'TAG'
+  "ID", "EX", "YEAR", "YRREX", "PEX", "NL", "TERM", "AGE", "MINAGE",
+  "D1RM", "D2RM", "ENOTE"
 )
-midcols <- sort(setdiff(names(res), c(coln, scols)))
-ord <- c(coln, midcols, sort(scols))  # soil columns in the back
-setcolorder(res, ord)
+setcolorder(res, coln)
 
 ## Convert year column to full years (not abbreviations) and to integers
-res[, YEAR := paste0(ifelse(YEAR %in% c('00', '11'), '20', '19'), YEAR)]
+res[, YEAR := paste0(ifelse(YEAR %in% c('00'), '20', '19'), YEAR)]
 res[, YEAR := as.integer(YEAR)]
-resDims <- dim(res)
 
-## Note: ENOTE99 is character, the rest are doubles
-ids <- allCols$enotes
-ids <- setdiff(ids, 'ENOTE99')
-enotes <- cextgr[, c('ID', ids), with=FALSE]
-enotes <- melt(enotes, measure=patterns("^ENOTE"),
-  value.name='ENOTE', variable.name='YEAR', na.rm=TRUE)
-enotes[, YEAR := stri_extract(YEAR, regex='[0-9]+')]
-enotes[, YEAR := as.numeric(paste0("19", YEAR))]
+## Convert to integers
+intCols <- c("YRREX", "PEX", "NL", "AGE", "MINAGE", "ENOTE")
+res[, intCols := lapply(.SD, as.integer), .SDcols=intCols, with=FALSE]
 
-## Ignore value of 6 in 1998
-enotes <- enotes[ENOTE < 6, ]
-tab <- dcast(enotes[,-1,with=FALSE], YEAR ~ ENOTE)[order(-YEAR)]
+## YRREX can just be an indicator variable, it is constant for each individual
+res[, YRREX := ifelse(YRREX == YEAR, 1, NA)]
 
-if (requireNamespace('d3heatmap', quietly = TRUE)) {
-  d3heatmap(tab[,-1,with=FALSE], Rowv=FALSE, Colv=FALSE, colors='Blues', revC=FALSE,
-    dendrogram='none', labRow=tab[['YEAR']], show_grid=2)
-}
-if (has_dt) {
-  datatable(tab, options=dtopts, 
-    caption='Table: Counts of each value of ENOTE/YEAR.')
-}
-## /* end enotes */
+## /* end cleanup */
 ##'
-##' ### ENOTE99
-##' Examine `ENOTE99` and the `NL99_[1-3]` variables.  `ENOTE99` was separated into
-##' the three `NL99` columns, so should be redundant.  There was only one plant that had
-##' three new leaders (`ID` 431).  This checks that the separated
-##' variables are as expected and plots:
-##'   1. The cumulative extension growth of all individuals with at least two new leaders.
-##'   2. The non-cumulative extension growth of the above.
-##'   3. The cumulative/non-cumulative ext. growth for the plant with 
-##' three leader changes, with vertical lines showing the events of leader changes.
+##' # Save
 ##'
-##+enote99
-## Convert ENOTE99 to NL columns
-nlcols <- allCols$nl99s
-tst <- cextgr[!is.na(ENOTE99), c('ENOTE99', nlcols), with=FALSE]
-tst[, paste0('N', 1:3) := lapply(tstrsplit(ENOTE99, ' ', fixed=TRUE), 
-  function(x) suppressWarnings(as.numeric(paste0('19', x))))]
-
-## Check new ones against NL
-chk <- tst[, all(N1 == NL99_1 & N2 == NL99_2 & N3 == NL99_3, na.rm=TRUE)]
-if (!chk) stop( 'ENOTE99 -> NL columns check failed' )  # no problems
-
-## Take a look at some of these multiple nl cols
-tst <- cextgr[!is.na(NL99_1) & !is.na(NL99_2), ]
-n3 <- tst[, sum(!is.na(NL99_3))]  # only one has three leader changes
-
-## Make a graphic to see the growth of these guys (19 of em)
-dat <- tst[, with(allCols, c(id, exs)), with=FALSE]
-dat <- melt(dat, id.vars=allCols$id, measure=allCols$exs, value.name='EX', 
-  variable.name='YEAR', na.rm=TRUE)
-dat[, YEAR := as.numeric(paste0('19', stri_extract(YEAR, regex='[0-9]+')))]
-
-## Prepare as time column followed by y-value columns
-out <- suppressWarnings(dcast(dat, YEAR ~ ID, fill='EX', value.var='EX'))
-
-## Plot 1: non-cumulative ext. growth for all indiv. with at least two NL99
-library(dygraphs)
-dygraph(out, xlab='Year', ylab='Extension Growth',
-  main='Extension Growth for indiv. with at least\nNL99_1 and NL99_2')
-
-## Get cumulative growth
-f <- function(x) c(rep(NA, sum(is.na(x))), cumsum(x[!is.na(x)]))
-res <- out[, lapply(.SD, f), .SDcols=setdiff(names(out), 'YEAR')][, YEAR:=out$YEAR]
-setcolorder(res, c('YEAR', setdiff(names(res), 'YEAR')))
-
-## Plot 2: cumulative
-dygraph(res, xlab='Year', ylab='Cumulative Extension Growth',
-  main='Extension Growth for indiv. with at least\nNL99_1 and NL99_2')
-
-## Get dates of events
-events <- tst[list(ID=dat[,unique(ID)]), 
-  c('ID', allCols$nl99s), on='ID', with=FALSE][ID == 431, ]
-
-dt <- out[, 1:2, with=FALSE][, `Cumulative Growth` := res[,2,with=FALSE]]
-setnames(dt, '431', 'Non-cumulative Growth')
-
-## Plot 3: growth with 3 NL
-dygraph(dt, ylab='Extension Growth', xlab='Year',
-  main='Extension Growth for only plant with 3 NLs (431)') %>%
-  dyEvent(events$NL99_1, 'Third new leader', color='red') %>%
-  dyEvent(events$NL99_2, 'Second new leader', color='red') %>%
-  dyEvent(events$NL99_3, 'First new leader', color='red')
-
-## /* end enote99 */
+##' Saving as ../temp/extgr.rda with bzip2 compression
 ##'
-##' ### Terminal conditions
-##' Tabulated values of `TERM99` and `TERM00`
+##' The first 300 rows of the result:
 ##'
-##+term, results='as.is'
-terminal <- melt(cextgr[, .(ID, TERM99, TERM00)], measure=patterns("^TERM"),
-  value.name='TERM', variable.name='YEAR')
-terminal[, YEAR := stri_extract(YEAR, regex='[0-9]+')]
-terminal[, YEAR := ifelse(YEAR == '00', '2000', '1999')]
+##+save, echo=FALSE
 
-tab <- table(terminal$TERM, terminal$YEAR)
-kable(tab, caption='Table: TERM labels by collection year.')
+## When saving:
+extgr <- res
+## save(extgr, file="../temp/extgr.rda", compress="bzip2")
 
-## /* end term */
+datatable(head(extgr, 300))
+
+## /* end save */
 ##'
-input <- list(temp='system2("echo", "Nice...")')
-commandString <- paste0("qplot(", input$temp, ", data=., geom='density')")
