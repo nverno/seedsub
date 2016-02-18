9fbc7889e820d0f95f79de8c1b8361ab58363081
Noah Peart <noah.v.peart@gmail.com>
2016-02-18 07:52:29 -0500

    extension growth data docked and added

---
+++ R/cextgr.R
@@ -0,0 +1,8 @@
+##' Contour segments seedling extension growth raw data.
+##'
+##' Raw extension growth data extracted from seesapmas11 master file.
+##'
+##' @format An object of class \code{data.table} (inherits from
+##'   \code{data.frame}) with 6701 rows and 102 columns.
+##' 
+"cextgr"
